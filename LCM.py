4e66d53e6d7f88bae91107500bb45add1e0ded13
isg99 <isaacg99@gmail.com>
2022-08-26 13:02:37 -0500

    METODO LINEAL CONGRUENCIAL hecho!

---
+++ LCM.py
@@ -0,0 +1,24 @@
+import numpy as np
+np.set_printoptions(suppress=True)
+
+print("METODO LINEAL CONGRUENCIAL\n")
+
+x0 = input("X0 = ")
+x0 = int(x0)
+a = input("a = ")
+a = int(a)
+c = input("c = ")
+c = int(c)
+m = input("m = ")
+m = int(m)
+cycles = input("ciclos = ")
+cycles = int(cycles)
+
+r = np.arange(cycles)
+
+for i in range(cycles):
+    x0 = (a*(x0) + c) % m
+    r = np.append(r, x0/m)
+
+print("RESULTADO: \n")
+print(r)
